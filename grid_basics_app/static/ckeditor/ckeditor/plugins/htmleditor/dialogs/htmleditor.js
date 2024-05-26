d696ecc6dce3f409fa337cf1b719f867ee2692bb
user <user@pc>
2024-05-26 20:26:42 +0300

    Доделал html редактор

--- grid_basics_app/static/ckeditor/ckeditor/plugins/htmleditor/dialogs/htmleditor.js
+++ grid_basics_app/static/ckeditor/ckeditor/plugins/htmleditor/dialogs/htmleditor.js
@@ -34,7 +34,7 @@ CKEDITOR.dialog.add('htmleditorDialog', function(editor) {
           editor.insertHtml(`
           <div class='html-editor d-flex flex-wrap flex-lg-nowrap gap-3 gap-lg-0'>
             <textarea class="form-control language-html" spellcheck="false">${markup_val}</textarea>
-            <iframe sandbox="allow-scripts" class="ratio border border-dark-subtle rounded-3" src=""></iframe> 
+            <iframe sandbox="allow-scripts" class="ratio border border-dark-subtle" src=""></iframe> 
           </div>
           `);
         }
--- grid_basics_app/static/js/html_editor.js
+++ grid_basics_app/static/js/html_editor.js
@@ -15,6 +15,22 @@ document.addEventListener("DOMContentLoaded", function(){
         let textarea = e.children[0];
         let iframe = e.children[1];
         textarea.addEventListener('input', function() { updateIframe(textarea, iframe) } );
+        //Разрешаем ввод символов табуляции
+        textarea.addEventListener('keydown', function(e) { 
+            if (e.key == 'Tab') {
+                e.preventDefault();
+                var start = this.selectionStart;
+                var end = this.selectionEnd;
+            
+                // set textarea value to: text before caret + tab + text after caret
+                this.value = this.value.substring(0, start) +
+                  "\t" + this.value.substring(end);
+            
+                // put caret at right position again
+                this.selectionStart =
+                  this.selectionEnd = start + 1;
+              }
+        });
         updateIframe(textarea, iframe);
         textarea.style.height = 0;
         textarea.style.height = textarea.scrollHeight + 'px';
