1c9bf6fb8a14dde691f8b4b87b3b6a6b57ffc113
user <user@pc>
2024-05-29 11:22:22 +0300

    Добавил кнопку открытия html редактора в новом окне + подправил код в некоторых редакторах

--- grid_basics_app/static/js/html_editor.js
+++ grid_basics_app/static/js/html_editor.js
@@ -1,11 +1,15 @@
 function updateIframe(textarea, iframe)
 {
-
     iframe = (iframe.contentWindow) ? iframe.contentWindow : (iframe.contentDocument.document) ? iframe.contentDocument.document : iframe.contentDocument;
     iframe.document.open();
     iframe.document.write(textarea.value);
     iframe.document.close();
     return false;
+}
+function openInNewWindow(textarea){
+    debugger;
+    var win = window.open("", "Example", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width="+screen.availWidth+",height="+screen.availHeight);
+    win.document.body.innerHTML = textarea.value;
 }
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -14,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
         let textarea = e.children[0];
         let iframe = e.children[1];
+        //Событие на ввод
         textarea.addEventListener('input', function() { updateIframe(textarea, iframe) } );
         //Разрешаем ввод символов табуляции
         textarea.addEventListener('keydown', function(e) { 
@@ -32,6 +37,11 @@ document.addEventListener("DOMContentLoaded", function(){
               }
         });
         updateIframe(textarea, iframe);
+        //Добавление кнопки открытия в новом окне
+        newWinBtn = document.createElement('button');
+        newWinBtn.addEventListener('click', function() { openInNewWindow(textarea); });
+        e.appendChild(newWinBtn);
+
         // textarea.style.height = 0;
         // textarea.style.height = textarea.scrollHeight + 'px';
     }
